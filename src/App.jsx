706e88aaa4137a294427151f1420e22888a4d818
Sydney Spaulding <cozysydmedia@gmail.com>
2024-02-15 17:56:36 -0800

    imported json info

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./user.css";
 import UserCard from "./UserCard";
 import name from "./user.json";
+import age from "./user.json";
+import phoneNumber from "./user.json";
+import address from "./user.json";
 
 function App() {
-  return <UserCard name={name} />;
+  return (
+    <UserCard
+      name={name}
+      age={age}
+      phoneNumber={phoneNumber}
+      address={address}
+    />
+  );
 }
 
 export default App;
